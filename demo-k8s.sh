7c8b42b0ee06ef3aadf96cbd22e9cc19d403d400
slist <stephane.list@gmail.com>
2022-03-04 11:00:25 +0100

    Add runtime demo

--- demo-k8s.sh
+++ demo-k8s.sh
@@ -4,20 +4,19 @@ RED='\033[1;31m'
 GREEN='\033[1;32m'
 NC='\033[0m' # No Color
 
-# Install necessary Ubuntu packages
-# Check if pv package is installed
-dpkg -s pv >/dev/null
-if [ $? -eq 1 ]
-then
-   sudo apt-get install -y pv
-fi
+apt_check_and_install () {
+   dpkg -s $1 >/dev/null
+   if [ $? -eq 1 ]
+   then
+      echo "Install $1"
+      sudo apt-get install -y $1
+   fi
+}
 
-# Check if meld package is installed
-dpkg -s meld >/dev/null
-if [ $? -eq 1 ]
-then
-   sudo apt-get install -y meld
-fi
+# Install necessary Ubuntu packages
+apt_check_and_install "pv"   # Used to simulate typing
+apt_check_and_install "meld" # Used to show differences between good/bad YAML files
+apt_check_and_install "git"  # Used to download netdemo
 
 # Check that cbctl is present in the PATH
 which cbctl >/dev/null
@@ -45,9 +44,9 @@ then
    intro="For this demo, we will use microK8s."
 fi
 
-########################
-# include the magic
-########################
+####################################
+###      include the magic       ###
+####################################
 . demo-magic.sh
 
 # pe  : Print and Execute.
@@ -55,6 +54,10 @@ fi
 # p   : Print only.
 # w   : wait
 # cmd : interactive mode
+
+#####################################
+###        PREPARE DEMO           ###
+#####################################
 
 clear
 echo "---"
@@ -68,6 +71,19 @@ kubectl delete service nodeapp-service >/dev/null 2>&1
 kubectl delete pod log4j >/dev/null 2>&1
 kubectl delete pod nginx >/dev/null 2>&1
 
+
+kubectl delete ns netdemo >/dev/null 2>&1
+kubectl delete service netdemo-service -n netdemo >/dev/null 2>&1
+kubectl delete configmaps netdemo-configmap -n netdemo >/dev/null 2>&1
+kubectl delete pod netdemo -n netdemo>/dev/null 2>&1
+
+rm -rf cb_demos >/dev/null 2>&1
+
+#####################################
+###        DEMO INTRO             ###
+#####################################
+
+demo_intro() {
 clear
 echo "---"
 echo -e "${RED}VMware Carbon Black Cloud Containers${NC} can protect:"
@@ -75,6 +91,11 @@ echo -e " - K8s onprem"
 echo -e " - K8s in public cloud, for example Amazon, Azure, or Google..."
 echo -e " - minikube"
 echo -e " - microK8s"
+
+# Depending on your customer, you can enable the following lines
+#echo -e " - OpenShift"
+#echo -e " - Rancher"
+
 echo -e " - and of course VMware Tanzu."
 
 if test -z "$intro" 
@@ -85,21 +106,25 @@ else
       echo ""
       echo ${intro}
 fi
-
 wait
 
-echo ""
+clear
 echo "---"
 echo -e "Let's check that CBC is ${GREEN}running${NC} in this K8s cluster."
 echo -e "How to check what pods are running in the K8s cluster?"
 pe "kubectl get pods -A"
-
 wait
-echo ""
-echo "---"
+echo -e ""
+echo -e "---"
 echo -e "CBC is running in it's own NAMESPACE ${GREEN}cbcontainers-dataplane${NC}."
 wait
+}
 
+#####################################
+###        DAEMONSET              ###
+#####################################
+
+demo_daemonset() {
 clear
 echo "---"
 echo -e "VMware CBC is deployed in each K8s cluster."
@@ -108,7 +133,13 @@ echo "It means that in a K8s cluster with CBC, on each K8s node, a CBC agent wil
 echo "Daemonsets are commonly used for monitoring, networking and security solutions."
 pe "kubectl get daemonsets -n cbcontainers-dataplane"
 wait
+}
 
+#####################################
+###         QUOTAS                ###
+#####################################
+
+demo_quota() {
 clear
 echo -e "---"
 echo -e "We would like to deploy a ${GREEN}node.js${NC} application called ${GREEN}nodeapp${NC}."
@@ -119,7 +150,6 @@ echo -e "Why a ${RED}minimum${NC} quota?"
 p "To be sure the pod will have enough CPU/mem to run correctly."
 echo -e "Why a ${RED}maximum${NC} quota?"
 p "To be sure the pod will not eat all CPU/mem, because of a bug or a cryptominer virus or someone playing Pacman..."
-
 wait
 clear
 echo -e "---"
@@ -180,7 +210,12 @@ echo "---"
 echo -e "So, what was wrong with the ${RED}BAD${NC} deployment file?"
 pe "cbctl k8s-object validate -f bad.yaml"
 wait
+}
+#####################################
+###          EXEC                 ###
+#####################################
 
+demo_exec() {
 clear
 echo "---"
 echo "Let's list all pods:"
@@ -200,6 +235,8 @@ wait
 
 clear
 echo "---"
+echo -e "${GREEN}Check logs in K8s violations.${NC}"
+echo "---"
 echo -e "Modify CBC K8s to ${RED}BLOCK --- COMMAND / Exec to container---${NC}"
 wait
 echo ""
@@ -209,25 +246,97 @@ echo -e "${RED}chmod +x fork-bomb.sh${NC}"
 echo -e "${RED}./fork-bomb.sh${NC}"
 pe "kubectl exec -it ${firstpod} -- /bin/bash"
 wait
+}
 
+#####################################
+###          LOG4J                ###
+#####################################
+
+demo_log4j() {
 clear
 echo "---"
 echo -e "Modify CBC K8s to ${RED}BLOCK --- CONTAINER IMAGES / Critical vulnerabilities ---${NC}"
 echo -e "Create an ${GREEN}exception${NC} if needed"
-
 wait
-echo "In our CICD pipeline we have integrated the automated vulnerability scan of all container images"
-echo -e "Let's try to deploy an image containing a ${RED}critical vulnerability${NC}"
-pe "kubectl run log4j --image=tamirmich/log4j2-demo:0.0.3"
-
-wait
-echo ""
-echo -e "Let's scan a container with the ${RED}log4j vulnerability${NC} ... it will be long"
+echo -e ""
+echo -e "---"
+echo -e "Let's scan a container with the ${RED}log4j vulnerability${NC}"
 pe "cbctl image scan tamirmich/log4j2-demo:0.0.3"
+wait
 
+echo ""
+echo -e "In our ${GREEN}CICD pipeline${NC} we have integrated the automated vulnerability scan of all container images"
+echo -e "Let's try to deploy the image containing the ${RED}log4j critical vulnerability${NC}"
+pe "kubectl run log4j --image=tamirmich/log4j2-demo:0.0.3"
+wait
+}
+
+#####################################
+###          GO PACKAGE           ###
+#####################################
+
+demo_go() {
+echo -e "---"
+echo -e "Let's scan a container with ${GREEN}GO${NC} packages"
+pe "cbctl image scan anchore/syft"
+wait
+}
+
+#####################################
+###          RUNTIME              ###
+#####################################
+
+demo_runtime() {
+clear
+echo -e "---"
+echo -e "In CBC UI, ${GREEN}unblock${NC} deployments with no CPU/mem quotas."
+echo -e ""
+
+wait
+echo -e "---"
+echo -e "In our K8s clusters, we can monitor/alert on ${RED}runtime network malicious activities${NC} such as:"
+echo " - Malicious IPs/URLs"
+echo " - Port scan"
+echo " - Baseline behavior"
+echo -e "Let's deploy an image to simulate ${RED}malicious network activities${NC}"
+
+pei "git clone https://github.com/0pens0/cb_demos.git"
+pei "cd cb_demos/netdemo/"
+pei "kubectl create namespace netdemo"
+pei "kubectl create -f deploy/. -n netdemo"
+pei "kubectl create -f k8s_service.yaml -n netdemo"
+pei "kubectl get services -n netdemo"
+
+echo -e "---"
+echo -e "Let's connect to the ${RED}malicious${NC} demo pod"
+port=`kubectl get services -n netdemo --no-headers | sed "s/.*://" | cut -f1 -d"/"`
+pe "firefox http://127.0.0.1:${port}"
+wait
+}
+
+demo_end() {
 echo ""
 echo ""
 echo -e "${GREEN}THANK YOU!${NC}"
 echo ""
 echo ""
+}
 
+#####################################
+###           MAIN                ###
+#####################################
+
+demo_intro
+demo_daemonset
+
+demo_runtime
+demo_log4j
+
+# Use this demo for DEV/SEC/OPS only
+#demo_quota
+#demo_exec
+#demo_go
+
+demo_end
+
+exit 0
